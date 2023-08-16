d7a344758b25fd0aa331f67705ac5da11c086767
bangoldx <bangold@proton.me>
2023-08-15 20:40:42 -0500

    added plant data, drop down menu, added images and blank files

---
+++ src/Banner.js
@@ -0,0 +1,8 @@
+function Banner(){
+    return(
+        <div>
+            <h1>Plant App Name</h1>
+        </div>
+    )
+}
+export default Banner;
---
+++ src/plantData.js
@@ -0,0 +1,93 @@
+const plantData = [
+    
+    {
+        id: 1,
+        name: "Ficus Ginseng Bonsai",
+        type: "Bonsai",
+        water: "Water thoroughly when top soil becomes slightly dry, lightly mist daily",
+        light: "4-5 hours of direct light",
+        temperature: "Consistantly 60F or higher and humid. Can tolerate dryer climates as well"
+    },
+    {
+        id: 2,
+        name: "White Fox Aloe",
+        type: "Succulant",
+        water: "Allow to completely dry between waterings",
+        light: "Minimum of 6 of indirect light",
+        temperature: "B"
+    },
+    {
+        id: 3,
+        name: "Frech Lavendar",
+        type: "Lavendar",
+        water: "Every 2-3 weeks",
+        light: "Direct, intense light",
+        temperature: "Does not handle frost, snow or freezing temperatures"
+    },
+    {
+        id: 4,
+        name: "Tropical Banana",
+        type: "Tree",
+        water: "Keep soil consistanly moist",
+        light: "At least 6 hours of direct light",
+        temperature: "Between 75F-95F"
+    },
+    {
+        id: 5,
+        name: "Hens and Chicks",
+        type: "Succulant",
+        water: "Once a week in warm climates, can go weeks between waterings",
+        light: "At least 6 hours of direct light",
+        temperature: "65F-75F"
+    },
+    {
+        id: 6,
+        name: "Wandering Jew",
+        type: "Ivy",
+        water: "Once weekly if top 1 inch of soil is dry, every other week during winter",
+        light: "Partial sun/shade",
+        temperature: "50F-85F"
+    },
+    {
+        id: 7,
+        name: "Purslane",
+        type: "Microgreen",
+        water: "Water when soil is dry but not parched",
+        light: "Full sun, can flower more if in partial shade",
+        temperature: "Good with hot"
+    },
+    {
+        id: 8,
+        name: "Lemon Balm",
+        type: "Mint",
+        water: "Err on side of underwatering, small amounts",
+        light: "5-6 hours of direct light, rotate to avoid sunburn",
+        temperature: "Avoid overly cool and overly hot temperatures"
+    },
+    {
+        id: 9,
+        name: "Catnip",
+        type: "Mint",
+        water: "Err on side of underwatering, small amounts",
+        light: "5-6 hours of direct light, rotate to avoid sunburn",
+        temperature: "Avoid overly cool and overly hot temperatures"
+    },
+    {
+        id: 10,
+        name: "Spearmint",
+        type: "Mint",
+        water: "Almost dry between waterings, then soak thoroughly",
+        light: "Full light to partial shade",
+        temperature: "Does not tolerate cold"
+    },
+    {
+        id: 11,
+        name: "Rosemary",
+        type: "Herb",
+        water: "Water when top couple inches are dry, keep moist but not soggy",
+        light: "At least 6 hours of direct sun, does not tolerate shade",
+        temperature: "30F-80F"
+    },
+]
+
+export default plantData;
